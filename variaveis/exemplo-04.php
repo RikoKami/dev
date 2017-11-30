26c35acd345329219520c54ce8ec72b713b41948
Riko <orglary@gmail.com>
2017-11-30 20:22:22 -0200

    Variaveis pre-definidas ou array super globais

--- variaveis/exemplo-04.php
+++ variaveis/exemplo-04.php
@@ -1,10 +1,24 @@
 <?php
 
-//  =========== Variáveis PRE-DEFINIDAS ou SUPER GLOBAIS ========== //
+//  =========== Variáveis PRE-DEFINIDAS ou ARRAYS SUPER GLOBAIS ========== //
 //$nome = $_GET["a"]; //sempre string
-$nome = (int)$_GET["a"]; //mudando tipo da variável
+//$nome = (int)$_GET["a"]; //mudando tipo da variável
 
-var_dump($nome);
+//Na url colocar ?a=123&b=456
+//var_dump($nome);
+
+
+// Pegando o IP do usuário
+$ip = $_SERVER["REMOTE_ADDR"];
+echo "Ip: " . $ip;
+
+
+
+echo "<br><br>";
+
+// Pegando o nome do arquivo
+$ipName = $_SERVER["SCRIPT_NAME"]; //nome do arquivo exibido: exemplo-04.php
+echo "Nome do arquivo: " . $ipName;
 
 
 ?>
